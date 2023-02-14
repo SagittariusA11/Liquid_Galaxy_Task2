1624f68fdcb8d9a5024c3737285387eb14c4b249
Manash Kumar <manashkumar1216@gmail.com>
2023-02-14 21:35:16 +0530

    configured_lgRig

---
+++ lib/kmlfiles/kml_files.dart
@@ -0,0 +1,424 @@
+class KML {
+
+  String name01 = "iitbhu";
+  String descp01 = '''
+  <?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+	<name>iitbhu.kml</name>
+	<StyleMap id="msn_ylw-pushpin">
+		<Pair>
+			<key>normal</key>
+			<styleUrl>#sn_ylw-pushpin</styleUrl>
+		</Pair>
+		<Pair>
+			<key>highlight</key>
+			<styleUrl>#sh_ylw-pushpin</styleUrl>
+		</Pair>
+	</StyleMap>
+	<Style id="sh_ylw-pushpin">
+		<IconStyle>
+			<scale>1.3</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Style id="sn_ylw-pushpin">
+		<IconStyle>
+			<scale>1.1</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Placemark>
+		<name>IIT (BHU) - Varanasi</name>
+		<LookAt>
+			<longitude>82.99366038693375</longitude>
+			<latitude>25.2655362538145</latitude>
+			<altitude>0</altitude>
+			<heading>2.412259465760821</heading>
+			<tilt>8.603376224459003</tilt>
+			<range>4687.179091316924</range>
+			<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+		</LookAt>
+		<styleUrl>#msn_ylw-pushpin</styleUrl>
+		<Polygon>
+			<tessellate>1</tessellate>
+			<outerBoundaryIs>
+				<LinearRing>
+					<coordinates>
+						83.00123279485558,25.2781681151048,0 82.99835683702007,25.27879146128146,0 82.99462418544991,25.27879002525849,0 82.99204706309092,25.27808682309367,0 82.98854333444943,25.27799826115599,0 82.98469953042211,25.27488344226763,0 82.98393388721479,25.27153602417494,0 82.98238583198209,25.26717633600999,0 82.98167273069899,25.26168054910791,0 82.98098700637111,25.25330259139632,0 82.9812403307092,25.25315558928095,0 82.98451944105223,25.25319139530716,0 82.9862648558309,25.25322896573163,0 82.98872047393019,25.25278957410483,0 82.98971491665179,25.25300930565336,0 82.99444479774232,25.25249638954573,0 82.99532923556409,25.25389041499657,0 82.99596906099094,25.25537140753499,0 82.99675373595818,25.25783967006648,0 82.99625593859369,25.25859415487767,0 82.99931173859366,25.27053259706091,0 83.0007725590275,25.27546635017814,0 83.00193143086885,25.27766901671769,0 83.00123279485558,25.2781681151048,0 
+					</coordinates>
+				</LinearRing>
+			</outerBoundaryIs>
+		</Polygon>
+	</Placemark>
+</Document>
+</kml>
+
+
+  ''';
+
+  String name02 = "harvard";
+  String descp02 = '''
+  <?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+	<name>harvard.kml</name>
+	<StyleMap id="msn_ylw-pushpin">
+		<Pair>
+			<key>normal</key>
+			<styleUrl>#sn_ylw-pushpin</styleUrl>
+		</Pair>
+		<Pair>
+			<key>highlight</key>
+			<styleUrl>#sh_ylw-pushpin</styleUrl>
+		</Pair>
+	</StyleMap>
+	<Style id="sh_ylw-pushpin">
+		<IconStyle>
+			<scale>1.3</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Style id="sn_ylw-pushpin">
+		<IconStyle>
+			<scale>1.1</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Placemark>
+		<name>Harvard</name>
+		<LookAt>
+			<longitude>-71.11767294970878</longitude>
+			<latitude>42.37700289581287</latitude>
+			<altitude>0</altitude>
+			<heading>-0.0001910662426275098</heading>
+			<tilt>0</tilt>
+			<range>1003.141987019541</range>
+			<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+		</LookAt>
+		<styleUrl>#msn_ylw-pushpin</styleUrl>
+		<Polygon>
+			<tessellate>1</tessellate>
+			<outerBoundaryIs>
+				<LinearRing>
+					<coordinates>
+						-71.1174204129818,42.37706417011906,0 -71.11741999876318,42.37686000692315,0 -71.11708367619809,42.37680102376704,0 -71.11705657210082,42.37705002995989,0 -71.1174204129818,42.37706417011906,0 
+					</coordinates>
+				</LinearRing>
+			</outerBoundaryIs>
+		</Polygon>
+	</Placemark>
+</Document>
+</kml>
+
+
+  ''';
+
+  String name03 = "iitb";
+  String descp03 = '''
+  <?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+	<name>iitb.kml</name>
+	<StyleMap id="msn_ylw-pushpin">
+		<Pair>
+			<key>normal</key>
+			<styleUrl>#sn_ylw-pushpin</styleUrl>
+		</Pair>
+		<Pair>
+			<key>highlight</key>
+			<styleUrl>#sh_ylw-pushpin</styleUrl>
+		</Pair>
+	</StyleMap>
+	<Style id="sh_ylw-pushpin">
+		<IconStyle>
+			<scale>1.3</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Style id="sn_ylw-pushpin">
+		<IconStyle>
+			<scale>1.1</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Placemark>
+		<name>iitb</name>
+		<LookAt>
+			<longitude>72.91321008516259</longitude>
+			<latitude>19.13350270949663</latitude>
+			<altitude>0</altitude>
+			<heading>-9.259910847722371e-06</heading>
+			<tilt>0.680331798608952</tilt>
+			<range>1004.704796485413</range>
+			<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+		</LookAt>
+		<styleUrl>#msn_ylw-pushpin</styleUrl>
+		<Polygon>
+			<tessellate>1</tessellate>
+			<outerBoundaryIs>
+				<LinearRing>
+					<coordinates>
+						72.91234637827671,19.13453390349493,0 72.91228108453709,19.13440444198696,0 72.91244742602615,19.13434289348304,0 72.91234637827671,19.13453390349493,0 
+					</coordinates>
+				</LinearRing>
+			</outerBoundaryIs>
+		</Polygon>
+	</Placemark>
+</Document>
+</kml>
+
+
+  ''';
+
+  String name04 = "mit";
+  String descp04 = '''
+  <?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+	<name>mit.kml</name>
+	<StyleMap id="msn_ylw-pushpin">
+		<Pair>
+			<key>normal</key>
+			<styleUrl>#sn_ylw-pushpin</styleUrl>
+		</Pair>
+		<Pair>
+			<key>highlight</key>
+			<styleUrl>#sh_ylw-pushpin</styleUrl>
+		</Pair>
+	</StyleMap>
+	<Style id="sh_ylw-pushpin">
+		<IconStyle>
+			<scale>1.3</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Style id="sn_ylw-pushpin">
+		<IconStyle>
+			<scale>1.1</scale>
+			<Icon>
+				<href>http://maps.google.com/mapfiles/kml/pushpin/ylw-pushpin.png</href>
+			</Icon>
+			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
+		</IconStyle>
+		<LineStyle>
+			<color>ffff0000</color>
+		</LineStyle>
+		<PolyStyle>
+			<color>8000aa00</color>
+		</PolyStyle>
+	</Style>
+	<Placemark>
+		<name>mit</name>
+		<LookAt>
+			<longitude>-71.09415999963701</longitude>
+			<latitude>42.36009099981992</latitude>
+			<altitude>0</altitude>
+			<heading>2.445782938951815e-10</heading>
+			<tilt>0</tilt>
+			<range>1000.000008301767</range>
+			<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+		</LookAt>
+		<styleUrl>#msn_ylw-pushpin</styleUrl>
+		<Polygon>
+			<tessellate>1</tessellate>
+			<outerBoundaryIs>
+				<LinearRing>
+					<coordinates>
+						-71.09186302930539,42.35920336004612,0 -71.09199199642318,42.35912910904581,0 -71.09180801418501,42.3591017230962,0 -71.09186302930539,42.35920336004612,0 
+					</coordinates>
+				</LinearRing>
+			</outerBoundaryIs>
+		</Polygon>
+	</Placemark>
+</Document>
+</kml>
+
+
+  ''';
+
+
+  iitbhu() {
+    return '''
+<?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    <Document>
+            <name>Network Links</name>
+          <visibility>0</visibility>
+          <open>0</open>
+          <description>Network link example 2</description>
+          <NetworkLink>
+            <name>View Centered Placemark</name>
+            <visibility>0</visibility>
+            <open>0</open>
+            <description>The view-based refresh allows the remote server to calculate
+                the center of your screen and return a placemark.</description>
+            <refreshVisibility>0</refreshVisibility>
+            <flyToView>0</flyToView>
+            <Link>
+              <href>http://lg1/cgi-bin/viewCenteredPlacemark.py</href>
+              <refreshInterval>2</refreshInterval>
+              <viewRefreshMode>onStop</viewRefreshMode>
+              <viewRefreshTime>1</viewRefreshTime>
+            </Link>
+          </NetworkLink>
+      <name>$name01</name>
+        $descp01
+    </Document>
+  </kml>
+''';
+  }
+  harvard() {
+    return '''
+<?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    <Document>
+            <name>Network Links</name>
+          <visibility>0</visibility>
+          <open>0</open>
+          <description>Network link example 2</description>
+          <NetworkLink>
+            <name>View Centered Placemark</name>
+            <visibility>0</visibility>
+            <open>0</open>
+            <description>The view-based refresh allows the remote server to calculate
+                the center of your screen and return a placemark.</description>
+            <refreshVisibility>0</refreshVisibility>
+            <flyToView>0</flyToView>
+            <Link>
+              <href>http://lg1/cgi-bin/viewCenteredPlacemark.py</href>
+              <refreshInterval>2</refreshInterval>
+              <viewRefreshMode>onStop</viewRefreshMode>
+              <viewRefreshTime>1</viewRefreshTime>
+            </Link>
+          </NetworkLink>
+      <name>$name02</name>
+        $descp02
+    </Document>
+  </kml>
+''';
+  }
+  iitb() {
+    return '''
+<?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    <Document>
+            <name>Network Links</name>
+          <visibility>0</visibility>
+          <open>0</open>
+          <description>Network link example 2</description>
+          <NetworkLink>
+            <name>View Centered Placemark</name>
+            <visibility>0</visibility>
+            <open>0</open>
+            <description>The view-based refresh allows the remote server to calculate
+                the center of your screen and return a placemark.</description>
+            <refreshVisibility>0</refreshVisibility>
+            <flyToView>0</flyToView>
+            <Link>
+              <href>http://lg1/cgi-bin/viewCenteredPlacemark.py</href>
+              <refreshInterval>2</refreshInterval>
+              <viewRefreshMode>onStop</viewRefreshMode>
+              <viewRefreshTime>1</viewRefreshTime>
+            </Link>
+          </NetworkLink>
+      <name>$name03</name>
+        $descp03
+    </Document>
+  </kml>
+''';
+  }
+  mit() {
+    return '''
+<?xml version="1.0" encoding="UTF-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    <Document>
+            <name>Network Links</name>
+          <visibility>0</visibility>
+          <open>0</open>
+          <description>Network link example 2</description>
+          <NetworkLink>
+            <name>View Centered Placemark</name>
+            <visibility>0</visibility>
+            <open>0</open>
+            <description>The view-based refresh allows the remote server to calculate
+                the center of your screen and return a placemark.</description>
+            <refreshVisibility>0</refreshVisibility>
+            <flyToView>0</flyToView>
+            <Link>
+              <href>http://lg1/cgi-bin/viewCenteredPlacemark.py</href>
+              <refreshInterval>2</refreshInterval>
+              <viewRefreshMode>onStop</viewRefreshMode>
+              <viewRefreshTime>1</viewRefreshTime>
+            </Link>
+          </NetworkLink>
+      <name>$name04</name>
+        $descp04
+    </Document>
+  </kml>
+''';
+  }
+}
--- lib/views/loginView.dart
+++ lib/views/loginView.dart
@@ -334,40 +334,8 @@ class LGConnection {
     );
     int rigs = 4;
     rigs = (int.parse(credencials['numberofrigs']) / 2).floor() + 2;
-    String openLogoKML = '''
-<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
-<Document>
-	<name>VolTrac</name>
-	<open>1</open>
-	<description>The logo it located in the bottom left hand corner</description>
-	<Folder>
-		<name>tags</name>
-		<Style>
-			<ListStyle>
-				<listItemType>checkHideChildren</listItemType>
-				<bgColor>00ffffff</bgColor>
-				<maxSnippetLines>2</maxSnippetLines>
-			</ListStyle>
-		</Style>
-		<ScreenOverlay id="abc">
-			<name>VolTrac</name>
-			<Icon>
-				<href>https://raw.githubusercontent.com/yashrajbharti/kml-images/main/alllogos.jpg</href>
-			</Icon>
-			<overlayXY x="0" y="1" xunits="fraction" yunits="fraction"/>
-			<screenXY x="0" y="0.98" xunits="fraction" yunits="fraction"/>
-			<rotationXY x="0" y="0" xunits="fraction" yunits="fraction"/>
-			<size x="0" y="0" xunits="pixels" yunits="fraction"/>
-		</ScreenOverlay>
-	</Folder>
-</Document>
-</kml>
-  ''';
     try {
       await client.connect();
-      await client
-          .execute("echo '$openLogoKML' > /var/www/html/kml/slave_$rigs.kml");
     } catch (e) {
       return Future.error(e);
     }
